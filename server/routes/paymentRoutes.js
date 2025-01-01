42dd53cc4f8fdbc4116b4498b51a604838d45cee
Vincent Sang <kiplangatsang08@gmail.com>
2025-01-02 01:47:54 +0300

    initiateSTKPush,   processCallback,   generateDarajaToken,

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -6,19 +6,13 @@ import {
   generateDarajaToken,
 } from "../controllers/paymentController.js";  // Include the .js extension
 
-import {
-  createCheckoutSession,
-  webhook,
-} from "../controllers/stripeController.js";  // Include the .js extension
-
 const router = express.Router();
 
 // Define routes
 router.post("/initiate-stk-push", generateDarajaToken, initiateSTKPush);
 router.post("/process-callback", processCallback);
 
-router.post("/create-checkout-session", createCheckoutSession);
-router.post("/webhook", express.json({ type: "application/json" }), webhook);
+
 
 // Export router as default
 export default router;
