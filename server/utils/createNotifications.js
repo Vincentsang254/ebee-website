4d345d45860d20a7bf4fa20cb26d0c44402a6d7d
Vincent Sang <kiplangatsang08@gmail.com>
2025-01-02 23:22:02 +0300

    import { Notifications  from "../models.js";  // Ensure to use the .js extension

--- server/utils/createNotifications.js
+++ server/utils/createNotifications.js
@@ -1,5 +1,6 @@
 // Import necessary models using ES6 import syntax
-import { Notifications } from "../models.js";  // Ensure to use the .js extension
+
+import Notifications from "../models/Notification.js";
 
 // Function to create a notification
 const createNotification = async (userId, type, content) => {
