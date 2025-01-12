05de60d6fd60498c2ecceb701f90c567d9245b95
Vincent Sang <kiplangatsang08@gmail.com>
2025-01-12 17:37:27 +0300

    haaa

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,21 +5,16 @@ import {
 	login,
 
 	signup,
-	verificationAccount,
-	sendVerificationCode,
 	forgotPassword,
 	changePassword,
-	verifyCode
 } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/verify/:verificationCode", verificationAccount);
-router.post("/send-code", sendVerificationCode);
+// router.post("/verify/:verificationCode", verificationAccount);
 router.post("/forgot-password", forgotPassword);
 router.put("/change-password", changePassword);
-router.put("/verify-code", verifyCode);
 
 export default router;
