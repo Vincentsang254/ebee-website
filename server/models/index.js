79006c7d90e23b54d26161f1046676a25a29eff0
Vincent Sang <kiplangatsang08@gmail.com>
2025-01-15 01:08:28 +0300

    mdls

--- server/models/index.js
+++ server/models/index.js
@@ -4,6 +4,7 @@ import { Sequelize, DataTypes } from 'sequelize';
 import { fileURLToPath } from 'url';
 import config from '../config/db.js';
 
+// Convert URL to path for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -31,17 +32,18 @@ const db = {};
 db.initializeModels = async () => {
   const modelFiles = fs.readdirSync(__dirname).filter(
     (file) =>
-      file.indexOf(".") !== 0 &&
-      file.slice(-3) === ".js" &&
-      file.indexOf(".test.js") === -1
+      file.indexOf('.') !== 0 &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
   );
 
   // Initialize models and add them to the db object
   for (const file of modelFiles) {
     const model = (await import(path.join(__dirname, file))).default;
 
+    // Ensure that each model calls `init()` correctly with Sequelize instance
     if (model && typeof model.init === 'function') {
-      model.init(sequelize, DataTypes); // Ensure passing sequelize and DataTypes
+      model.init(sequelize, DataTypes); // Ensure passing sequelize and DataTypes to the init function
       db[model.name] = model; // Store the model in the db object
     }
   }
