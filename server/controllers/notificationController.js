3b7f22f20ca16e6f7ea2ea9466176a10daca6a84
Vincent Sang <kiplangatsang08@gmail.com>
2025-02-08 15:19:02 +0300

    adding sequelize..

--- server/controllers/notificationController.js
+++ server/controllers/notificationController.js
@@ -1,50 +1,54 @@
 /** @format */
 
-const {Notifications} = require("../models");
+const { Notifications } = require("../models");
 export const getNotifications = async (req, res) => {
-	try {
-		const userId = req.user.id; 
+  try {
+    const userId = req.user.id;
 
-		const notifications = await Notifications.findAll({
-			where: { userId },
-			order: [["createdAt", "DESC"]], // Sort by latest first
-		});
+    const notifications = await Notifications.findAll({
+      where: { userId },
+      order: [["createdAt", "DESC"]], // Sort by latest first
+    });
 
-		res.status(200).json({ status: true, data: notifications });
-	} catch (error) {
-		res.status(500).json({ status: false, message: error.message });
-	}
+    res.status(200).json({ status: true, data: notifications });
+  } catch (error) {
+    res.status(500).json({ status: false, message: error.message });
+  }
 };
 
 export const markAsRead = async (req, res) => {
-	try {
-		const notificationId = req.params.notificationId;
-		const notification = await Notifications.findByPk(notificationId);
+  try {
+    const notificationId = req.params.notificationId;
+    const notification = await Notifications.findByPk(notificationId);
 
-		if (!notification) {
-			return res.status(404).json({status: false, message: "Notification not found" });
-		}
+    if (!notification) {
+      return res
+        .status(404)
+        .json({ status: false, message: "Notification not found" });
+    }
 
-		notification.is_read = true;
-		await notification.save();
+    notification.is_read = true;
+    await notification.save();
 
-		res.status(200).json({status: true, message: "Notification marked as read" });
-	} catch (error) {
-		res.status(500).json({status: false, message: error.message });
-	}
+    res
+      .status(200)
+      .json({ status: true, message: "Notification marked as read" });
+  } catch (error) {
+    res.status(500).json({ status: false, message: error.message });
+  }
 };
 
 export const getNots = async (req, res) => {
-	try {
-		const notifications = await Notifications.findAll();
-		res.status(200).json({status: false, data: notifications});
-	} catch (error) {
-		res.status(500).json({status: false, message: error.message });
-	}
+  try {
+    const notifications = await Notifications.findAll();
+    res.status(200).json({ status: false, data: notifications });
+  } catch (error) {
+    res.status(500).json({ status: false, message: error.message });
+  }
 };
 
 module.expors = {
-	getNotifications,
-	markAsRead,
-	getNots,
-}
+  getNotifications,
+  markAsRead,
+  getNots,
+};
